cd9fc7ae5c6358023346e82833bf368ab14594c8
profe.dam2 <profe.dam2@gmail.com>
2025-10-16 10:04:17 +0200

    Sincronizar cambios para modelo proyecto

--- models/models.py
+++ models/models.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class modulo_demo(models.Model):
-#     _name = 'modulo_demo.modulo_demo'
-#     _description = 'modulo_demo.modulo_demo'
+class departamento(models.Model):
+    _name = 'modulo_demo.departamento'
+    _description = 'Define los atributos de un departamento'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    # Atributos
+    nombreDpto = fields.Char(string='Nombre departamento' ,required=True)
 
+class empleado(models.Model):
+    _name = 'modulo_demo.empleado'
+    _description = 'Define los atributos de un empleado'
+
+    # Atributos
+    dniEmpleado = fields.Char(string='DNI' ,required=True)
+    nombreEmpleado = fields.Char(string='Nombre' ,required=True)
+    fechaNacimiento = fields.Datetime(string='Fecha de nacimiento' ,required=True, default=fields.date.today())
+    direccionEmpleado = fields.Char(string='Dirección')
+
+class proyecto(models.Model):
+    _name = 'modulo_demo.proyecto'
+    _description = 'Define los atributos de un proyecto'
+
+    #Atributos
+    nombreProyecto = fields.Char(string='Nombre proyecto' ,required=True)
+    tipoProyecto = fields.Char(string='Tipo proyecto', selection=[('f','Front-End'),('b','Back-End')], help='Tipo de proyecto' ,required=True)
+    descriptionProyect = fields.Text(string='Descripción del proyecto')
